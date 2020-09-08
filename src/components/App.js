430853116cf6fcd3207329fe8423963a716b4113
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-08 14:18:09 +0100

    using hooks to create request and reusing the request function for another component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import ResourceList from "./ResourceList.js";
+import UserList from "./UserList.js";
 
 const App = () => {
   const [resource, setResource] = useState("posts");
 
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>Todos</button>
